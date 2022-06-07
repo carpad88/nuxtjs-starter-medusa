9220a1bc78f1ff48e6c9bb977276fa65a3c69128
Carlos Padilla <carpad88@gmail.com>
2022-06-06 23:19:52 -0500

    refactor: add MEDUSA_BACKEND_URL env variable

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:9000/store'
+    baseURL: process.env.MEDUSA_BACKEND_URL || 'http://localhost:9000/store'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
